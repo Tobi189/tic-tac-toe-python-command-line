e34a1aa6dd7dfce02e6c729e5898b09a3a3b2a37
Nika K <kirvalidzenika33@gmail.com>
2025-06-05 17:52:46 +0400

    commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ turn = 'x'
 board = [None for _ in range(9)]
 game_over = False
 move = None
-
+winner = False
 def draw_board():
     for i in range(3):
         row = []
@@ -14,7 +14,7 @@ def draw_board():
             print('---------')
 
 def make_move():
-    global move, turn
+    global turn
     valid = False
     while not valid:
         print(f"{turn}'s turn")
@@ -43,13 +43,61 @@ def choose_first_player():
         choice = input('Your input here: ').strip()
     turn = 'x' if choice == '1' else 'o'
 
+def check_winner():
+    global board
+    winner = False
+    for i in range(3):
+        if board[3*i] and (board[3*i] == board[3*i+1] and board[3*i] == board[3*i+2]):
+            print(f'{board[3*i]} is the winner!')
+            winner = True
+        if board[i] and (board[i] == board[i+3] and board[i] == board[i+6]):
+            print(f'{board[i]} is the winner!')
+            winner = True
+    if board[0] and (board[0] == board[4] and board[0] == board[8]):
+        print(f'{board[0]} is the winner!')
+        winner = True
+    if board[2] and (board[2] == board[4] and board[2] == board[6]):
+        print(f'{board[2]} is the winner!')
+        winner = True
+
+    if winner:
+        while True:
+            answer = input('Do you want to play again y/n: ').strip()
+            if len(answer) != 1 or (answer.lower() != 'n' and answer.lower() != 'y'):
+                print('Please enter valid input')
+                continue
+            if answer.lower() == 'n':
+                exit()
+            else:
+                board = [None for _ in range(9)]
+                game_loop()
+            break
+    elif all(cell is not None for cell in board):
+        print("It's a draw!")
+        while True:
+            answer = input('Do you want to play again y/n: ').strip()
+            if len(answer) != 1 or (answer.lower() != 'n' and answer.lower() != 'y'):
+                print('Please enter valid input')
+                continue
+            if answer.lower() == 'n':
+                exit()
+            else:
+                board = [None for _ in range(9)]
+                game_loop()
+            break
+
+
+
 # Example game loop (you can expand this with win/draw detection)
-choose_first_player()
-draw_board()
-for _ in range(9):
-    make_move()
+
+def game_loop():
+    choose_first_player()
+    draw_board()
+    for _ in range(9):
+        make_move()
+        check_winner()
 
 
-
+game_loop()
 
 
